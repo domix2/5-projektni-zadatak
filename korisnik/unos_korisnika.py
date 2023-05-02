ce826b499ab29629f5d8627624b743a226dc915c
Domagoj <domagoj.speljko@gmail.com>
2023-05-02 12:23:24 +0200

    4 projektni zadatak

--- korisnik/unos_korisnika.py
+++ korisnik/unos_korisnika.py
@@ -1,5 +1,6 @@
 from utillities import unos_telefona
 def unos_korisnika(redni_broj):
+
     korisnik = {}
     korisnik['ime'] = input(f'Unesite ime {redni_broj}. korisnika:').capitalize()
     korisnik['prezime'] = input(f'Unesite prezime {redni_broj}. korisnika:').capitalize()
